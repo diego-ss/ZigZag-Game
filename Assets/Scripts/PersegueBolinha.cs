bc83242cac90ffffeb9b307cb3e539bd4812240a
Diego <diego_sousa.s@outlook.com>
2023-10-21 22:46:48 -0300

    feat: camera persegue bolinha com interpolação linear

---
+++ Assets/Scripts/PersegueBolinha.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersegueBolinha : MonoBehaviour
+{
+    [SerializeField]
+    private Transform bolinha;
+    [SerializeField]
+    private Vector3 distancia;
+    [SerializeField]
+    private float lerpValue;
+    [SerializeField]
+    private Vector3 posicaoCamera, posicaoAlvo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posicaoCamera = transform.position;
+        posicaoAlvo = bolinha.position;
+        distancia = posicaoAlvo - posicaoCamera;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        Perseguir();
+    }
+
+    private void Perseguir()
+    {
+        posicaoAlvo = bolinha.position;
+        posicaoCamera = Vector3.Lerp(posicaoCamera, posicaoAlvo - distancia, lerpValue * Time.deltaTime);
+        transform.position = posicaoCamera;
+    }
+}
